02d4022688b313d794bd1ee5d0f016a5de7bb528
haoScoville <hao@sc0ville.com>
2020-02-06 17:11:06 +0700

    Postgres now accept config files

--- database/start.sh
+++ database/start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
+cat ./postgresql.conf > /var/lib/postgresql/data/postgresql.conf
+cat ./pg_hba.conf > /var/lib/postgresql/data/pg_hba.conf
 psql -d ${POSTGRES_DB} -a -U ${POSTGRES_USER} -f ./schema.sql
 psql -d ${POSTGRES_DB} -a  -U${POSTGRES_USER} -f ./data.sql
 
